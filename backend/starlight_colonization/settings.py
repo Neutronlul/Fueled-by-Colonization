a2d4c2e061130e6330a9eb28ed6472e894d01389
Neutronlul <ultiphoton@gmail.com>
2025-03-18 20:52:39 -0700

    Messed with DRF idk

--- backend/starlight_colonization/settings.py
+++ backend/starlight_colonization/settings.py
@@ -40,10 +40,15 @@ INSTALLED_APPS = [
 
     'rest_framework',
 
+    'corsheaders',
+
     'polls.apps.PollsConfig',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -125,3 +130,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
+CORS_ALLOWED_ORIGINS = [
+    'https://neutronlul.github.io',
+    'http://localhost:8000'
+]
